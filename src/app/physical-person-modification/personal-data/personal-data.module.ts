a537ecbc79aa5bf68e556ff5ca7e26d2adbfd45a
ricardfiguls <ricard.figuls@gmail.com>
2018-06-16 21:01:44 +0200

    wip

--- src/app/physical-person-modification/personal-data/personal-data.module.ts
+++ src/app/physical-person-modification/personal-data/personal-data.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PersonalDataComponent } from './personal-data.component';
 import { MyInputModule } from '../../my-input/my-input.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MySsoModule } from '../../my-sso/my-sso.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MyInputModule,
+    MySsoModule,
     ReactiveFormsModule,
   ],
   declarations: [PersonalDataComponent],
