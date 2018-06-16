ac09df6a6ca4b8283d7b8ad503df4b99faeec2d9
ricardfiguls <ricard.figuls@gmail.com>
2018-06-16 19:17:54 +0200

    Base Component extended

---
+++ src/app/my-base-component.ts
@@ -0,0 +1,56 @@
+import { Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
+
+export class MyBaseComponent implements OnInit {
+  @Input() name: string;
+  @Input() title: string;
+  @Input() placeholder: string;
+  @Input() attachedFormGroup: FormGroup;
+  @Input() required = false;
+  @Input() readOnly = false;
+  @Input() maxLength: number;
+  @Input() minLength: number;
+  public formControl = new FormControl();
+  private validators = [];
+  private myControl: AbstractControl;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.configure();
+  }
+
+  public hasError() {
+    return this.attachedFormGroup.controls[this.name].errors !== null;
+  }
+
+  public getError() {
+    return this.attachedFormGroup.controls[this.name].errors;
+  }
+
+  private configure() {
+    this.setValidators();
+    this.createModel();
+  }
+
+  private setValidators() {
+    this.validators = [];
+    if (this.required) {
+      this.validators.push(Validators.required);
+    }
+    if (this.maxLength) {
+      this.validators.push(Validators.maxLength(this.maxLength));
+    }
+    if (this.maxLength) {
+      this.validators.push(Validators.minLength(this.minLength));
+    }
+  }
+
+  private createModel() {
+    if (!this.attachedFormGroup.contains(this.name)) {
+      this.attachedFormGroup.addControl(this.name, new FormControl(null, this.validators));
+      this.myControl = this.attachedFormGroup.get(this.name);
+    }
+  }
+
+}
--- src/app/my-input-module/my-input-module.component.ts
+++ src/app/my-input-module/my-input-module.component.ts
@@ -1,39 +1,19 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { MyBaseComponent } from '../my-base-component';
 
 @Component({
   selector: 'app-my-input-module',
   templateUrl: './my-input-module.component.html',
   styleUrls: ['./my-input-module.component.css']
 })
-export class MyInputModuleComponent implements OnInit {
-  @ViewChild('input') input: ElementRef;
-  @Input() name: string;
-  @Input() title: string;
-  @Input() placeholder: string;
-  @Input() attachedFormGroup: FormGroup;
-  @Input() mandatory = false;
-  public formControl = new FormControl();
-  private validators = [];
-  private myControl: AbstractControl;
+export class MyInputModuleComponent extends MyBaseComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
-    if (this.mandatory) {
-      this.validators.push(Validators.required);
-    }
-    if (!this.attachedFormGroup.contains(this.name)) {
-      this.attachedFormGroup.addControl(this.name, new FormControl(null, this.validators));
-      this.myControl = this.attachedFormGroup.get(this.name);
-    }
-  }
-
-  public hasError() {
-    return !this.attachedFormGroup.controls[this.name].errors === null;
-  }
-
-  public test() {
-    console.log(this.attachedFormGroup.controls[this.name]);
+    super.ngOnInit();
   }
 }
