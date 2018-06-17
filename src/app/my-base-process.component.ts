2a6eb0f8d849ce06c4106c66e26ef43b52d79299
ricardfiguls <ricard.figuls@gmail.com>
2018-06-17 15:15:03 +0200

    added base process extension

---
+++ src/app/my-base-process.component.ts
@@ -0,0 +1,102 @@
+import { OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+export class MyBaseProcess implements OnInit {
+  public modelForm: FormGroup;
+  public modelOriginal;
+
+  ngOnInit(): void {
+  }
+
+  public saveModel(): void {
+    if (this.modelForm.status !== 'VALID') {
+      this.touchAll();
+      alert('Validation errors detected!\nErrors must be fixed.');
+    } else {
+      console.log(this.modelForm.value);
+      this.modelOriginal = this.modelForm.value;
+      this.untouch();
+      alert('Data saved!');
+    }
+  }
+
+  public cancel(): void {
+    if (this.modelForm.dirty) {
+      alert('You have unsaved changes. Are you sure you want to cancel/leave?');
+    } else {
+      alert('No changes detected, you can leave!');
+    }
+  }
+
+  public clearModel(): void {
+    this.modelForm.reset();
+    this.modelOriginal = this.modelForm.value;
+  }
+
+  public resetModel() {
+    this.modelForm.reset();
+    this.modelForm.setValue(this.modelOriginal);
+    this.modelOriginal = this.modelForm.value;
+    this.untouch();
+  }
+
+  public showModel(): void {
+    console.log(this.modelForm);
+  }
+
+  public untouch() {
+    this.modelForm.markAsUntouched();
+    this.modelForm.markAsPristine();
+  }
+
+  public adaptModelValues(model: FormGroup | FormControl, values) {
+    this.adaptValues(model, values);
+    this.adaptModel(model, values);
+  }
+
+  private adaptModel(model: FormGroup | FormControl, values) {
+    // Adding keys to value from model
+    Object.keys(model.controls).forEach(control => {
+      if (model.controls[control] instanceof FormGroup) {
+        if (values[control] === undefined) {
+          values[control] = model.value[control];
+        }
+        this.adaptModel(model.controls[control], values[control]);
+      }
+      if (model.controls[control] instanceof FormControl) {
+        if (values[control] === undefined) {
+          values[control] = '';
+        }
+      }
+    });
+  }
+
+  private adaptValues(model: FormGroup | FormControl, values ) {
+    // Deleting values that not exists in model
+    Object.keys(values).forEach(key => {
+      if (typeof values[key] === 'object') {
+        this.adaptValues(model.controls[key], values[key]);
+      } else {
+        if (model.get(key) === null) {
+          delete values[key];
+        }
+      }
+    });
+  }
+
+  private touchAll() {
+    this.markFormGroupTouched(this.modelForm);
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    (<any>Object).values(formGroup.controls).forEach(control => {
+      if (control.controls) {
+        this.markFormGroupTouched(control);
+      } else {
+        control.markAsTouched();
+      }
+    });
+   }
+}
+
+
--- src/app/physical-person-modification/physical-person-modification.component.ts
+++ src/app/physical-person-modification/physical-person-modification.component.ts
@@ -1,47 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MyBaseProcess } from '../my-base-process.component';
 
 @Component({
   selector: 'app-physical-person-modification',
   templateUrl: './physical-person-modification.component.html',
   styleUrls: ['./physical-person-modification.component.css']
 })
-export class PhysicalPersonModificationComponent implements OnInit {
-  public modelForm: FormGroup;
-  public modelOriginal;
+export class PhysicalPersonModificationComponent extends MyBaseProcess implements OnInit, AfterViewInit {
 
-  constructor() {}
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
+    super.ngOnInit();
     this.modelForm = new FormGroup({
       personalData: new FormGroup({}),
       AddressNotification: new FormGroup({})
     });
   }
 
-  public saveModel(): void {
-    if (this.modelForm.status !== 'VALID') {
-      this.touchAll();
-      alert('Validation errors detected!\nErrors must be fixed.');
-    } else {
-      console.log(this.modelForm.value);
-      this.modelOriginal = this.modelForm.value;
-      this.untouch();
-      alert('Data saved!');
-    }
-  }
-
-  public cancel(): void {
-    if (this.modelForm.dirty) {
-      alert('You have unsaved changes. Are you sure you want to cancel/leave?');
-    } else {
-      alert('No changes detected, you can leave!');
-    }
-  }
-
-  public clearModel(): void {
-    this.modelForm.reset();
-    this.modelOriginal = this.modelForm.value;
+  ngAfterViewInit(): void {
+    this.clearModel();
   }
 
   public loadModel(): void {
@@ -49,51 +30,21 @@ export class PhysicalPersonModificationComponent implements OnInit {
       personalData: {
         CAM_PFS_NOM: 'Chuck',
         CAM_PFS_APE_1: 'Norrys',
+        // CAM_PFS_APE_2: NOT DEFINED. ERROR?
         CAM_PFS_EDAT: '69',
+        CAM_PFS_IEXISTENT: 'MUST IGNORE THIS FIELD. ERROR?',
         CAM_PFS_SSO: { PRO: '12', NUM: '99999999', DIG: '12' }
+        // CAM_PFS_SSO2: NOT DEFINED. ERROR?
       },
       AddressNotification: {
         CAM_PFS_TIP_VIA: 'El Vecino',
         CAM_PFS_NOM_VIA: 'El Alcalde'
       }
     };
-    this.adaptModel(this.modelForm, value);
+    this.adaptModelValues(this.modelForm, value);
     this.modelForm.setValue(value);
 
     this.modelOriginal = this.modelForm.value;
     this.untouch();
   }
-
-  public resetModel() {
-    this.modelForm.setValue(this.modelOriginal);
-    this.untouch();
-  }
-
-  public showModel(): void {
-    console.log(this.modelForm);
-  }
-
-  private untouch() {
-    this.modelForm.markAsUntouched();
-    this.modelForm.markAsPristine();
-  }
-
-  private adaptModel(model: FormGroup | FormControl, values) {
-    Object.keys(model.controls).forEach(control => {
-      if (model.controls[control] instanceof FormGroup) {
-        if (values[control] === undefined) {
-          values[control] = model.value[control];
-        }
-        this.adaptModel(model.controls[control], values[control]);
-      }
-      if (model.controls[control] instanceof FormControl) {
-        if (values[control] === undefined) {
-          values[control] = '';
-        }
-      }
-    });
-  }
-
-  // TODO: Enable visibility of all errors
-  private touchAll() {}
 }
